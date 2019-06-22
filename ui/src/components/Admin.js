0cea02d6295c22c1f261b52e8bc30294f64ee920
ilzheev <anton@lovedesign.ru>
2019-06-22 11:43:08 +0400

    UI: Router

--- ui/src/components/Admin.js
+++ ui/src/components/Admin.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 
 import { Menu, Icon, Layout } from 'antd';
@@ -9,59 +10,97 @@ const { Header, Content, Footer } = Layout;
 
 const Admin = props => {
 
-    const [currentMenu, setCurrentMenu] = useState(["dashboard"]);
+    var currentLocation = window.location.pathname;
+    console.log(currentLocation);
+
+    const [currentMenu, setCurrentMenu] = useState([currentLocation]);
 
     const handleMenuClick = e => {
         setCurrentMenu([e.key]);
     };
 
     const logout = () => {
-        
         axios.get("/admin/logout")
         .then(function (response) {
           props.setLoggedIn(false)
         });
-  
     }
 
+    const Dashboard = () => (
+        <div>
+        Home
+        </div>
+    )
+
+    const Users = () => (
+        <div>
+        Users
+        </div>
+    )
+
+    const Queue = () => (
+        <div>
+        Queue
+        </div>
+    )
+
+    const Settings = () => (
+        <div>
+        Settings
+        </div>
+    )
+
     return (
-        <Layout>
-            <Header style={{ padding: 0, margin: 0, background: '#fff', height: 48 }}>
-                <Menu onClick={handleMenuClick} selectedKeys={currentMenu} mode="horizontal">
-                    <Logo />
-                    <Menu.Item key="dashboard">
-                        <Icon type="appstore" />
-                        Dashboard
-                    </Menu.Item>
-                    <Menu.Item key="users">
-                        <Icon type="team" />
-                        Users
-                    </Menu.Item>
-                    <Menu.Item key="queue">
-                        <Icon type="inbox" />
-                        Queue
-                    </Menu.Item>
-                    <Menu.Item key="settings">
-                        <Icon type="setting" />
-                        Settings
-                    </Menu.Item>
-                    <Menu.Item key="logout" onClick={logout} className="menu-logout">
-                        <Icon type="logout" />
-                        Logout
-                    </Menu.Item>
-                </Menu>
-            </Header>
-            <Content style={{ padding: 24, margin: 0 }}>
-                Content
-            </Content>
-            <Footer style={{ padding: '18px 24px', margin: 0, background: '#fff' }}>
-                <small>
-                    <code>
-                        Version: <Version />
-                    </code>
-                </small>
-            </Footer>
-        </Layout>
+        <Router>
+            <Layout>
+                <Header style={{ padding: 0, margin: 0, background: '#fff', height: 48 }}>
+                    <Menu onClick={handleMenuClick} selectedKeys={currentMenu} mode="horizontal">
+                        <Logo />
+                        <Menu.Item key="/dashboard">
+                            <Link to="/dashboard">
+                                <Icon type="appstore" />
+                                Dashboard
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/users">
+                            <Link to="/users">
+                                <Icon type="team" />
+                                Users
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/queue">
+                            <Link to="/queue">
+                                <Icon type="inbox" />
+                                Queue
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/settings">
+                            <Link to="/settings">
+                                <Icon type="setting" />
+                                Settings
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="/logout" onClick={logout} className="menu-logout">
+                            <Icon type="logout" />
+                            Logout
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: 24, margin: 0 }}>
+                        <Route exact path="/dashboard" component={Dashboard} />
+                        <Route exact path="/users" component={Users} />
+                        <Route exact path="/queue" component={Queue} />
+                        <Route exact path="/settings" component={Settings} />
+                </Content>
+                <Footer style={{ padding: '18px 24px', margin: 0, background: '#fff' }}>
+                    <small>
+                        <code>
+                            Version: <Version />
+                        </code>
+                    </small>
+                </Footer>
+            </Layout>
+        </Router>
     );
     
 };
