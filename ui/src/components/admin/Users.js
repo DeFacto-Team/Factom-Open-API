9335edf691ddaeaa758a1412a852e5860dd4daf2
ilzheev <anton@lovedesign.ru>
2019-07-08 16:52:01 +0400

    remove garbage

--- ui/src/components/admin/Users.js
+++ ui/src/components/admin/Users.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { Typography, Modal, Button, Icon, Table, Divider, Input, Popconfirm, Tooltip, message, Tag, Form } from 'antd';
+import { Typography, Button, Icon, Table, Divider, Input, Popconfirm, Tooltip, message, Tag, Form } from 'antd';
 import { NotifyNetworkError } from './../common/Notifications';
 import EditableText from './../common/EditableText';
 
