49f6c743aeec9a7958f2f12ff94d9645ace3a67a
ilzheev <anton@lovedesign.ru>
2019-07-23 18:24:56 +0400

    Admin UI: Dashboard template

--- ui/src/components/admin/Dashboard.js
+++ ui/src/components/admin/Dashboard.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import {
+  Typography,
+  Row,
+  Col,
+  Statistic,
+  Icon
+} from 'antd';
+
+const { Title, Paragraph } = Typography;
+
 const Dashboard = () => {
-  return <span>Dashboard</span>;
+  return (
+    <div>
+      <Title level={3}>Dashboard</Title>
+      <Paragraph type="secondary"><Icon type="info-circle" theme="twoTone" /> Dashboard is in development</Paragraph>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Statistic title="EC balance" value={0} />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Dashboard;
